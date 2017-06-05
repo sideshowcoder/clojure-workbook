c382aace98e9bc3a5267cd71897b6b99d4380c18
Philipp Fehre <philipp@fehre.co.uk>
2017-06-05 09:36:25 +0100

    playing with concurrency

--- src/clojure_workbook/chapter_10.clj
+++ src/clojure_workbook/chapter_10.clj
@@ -1,6 +1,48 @@
-(ns clojure-workbook.chapter-10)
+(ns clojure-workbook.chapter-10
+  (:require [clojure.string :as str]))
 
 (let [a (atom 0)]
   (dotimes [_ 10]
     (swap! a inc))
   @a)
+
+
+(defn get-quote-promise
+  []
+  (let [quote-promise (promise)]
+    (future (deliver quote-promise (slurp "http://www.braveclojure.com/random-quote")))
+    quote-promise))
+
+(defn word-count
+  [s]
+  (let [words (->> (str/split s #" ")
+                   (map #(str/replace %1 #"\.|\?|!|--" ""))
+                   (map str/trim)
+                   (filter not-empty)
+                   (map str/lower-case))]
+    (reduce-kv #(assoc %1 %2 (count %3)) {}
+               (group-by identity words))))
+
+(defn aggregate-word-count
+  [count-map aggregate]
+  (reduce-kv
+   (fn [_ k v]
+     (swap! aggregate (fn [c] (update-in c [k] #(+ (if (nil? %1) 0 %1) v)))))
+   nil
+   count-map))
+
+(aggregate-word-count (word-count "foo foo bar") word-counter)
+
+(defn process-quotes
+  [f count]
+  (let [quote-promises (map (fn [_] (get-quote-promise)) (range count))]
+    (pmap (fn [quote-promise] (f @quote-promise)) quote-promises)))
+
+
+(defn quote-word-count
+  [n]
+  (let [counter (atom {})]
+    (doall (process-quotes (fn [quote] (aggregate-word-count (word-count quote) counter)) n))
+    @counter))
+
+(time (quote-word-count 100))
